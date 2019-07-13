5f7e6fff8d3d2dc1cd4cc56156438e5ddedc1552
Jean-Benoist Leger <jb@leger.tf>
2019-07-13 15:48:43 +0200

    use constants for lcd4

--- _lcd4.py
+++ _lcd4.py
@@ -4,31 +4,10 @@ import collections
 
 
 def lcd4(
-    *,
-    prefix,
-    data_port,
-    data_pins4567,
-    e_port,
-    e_pin,
-    rs_port,
-    rs_pin,
-    header=True,
+    *, prefix, data_port, data_pins4567, e_port, e_pin, rs_port, rs_pin, header=True
 ):
-    dico = collections.OrderedDict(
-        {
-            "LCD_DATA_PORT": data_port,
-            "LCD_DATA_PIN4": f"PIN{data_pins4567[0]}_bm",
-            "LCD_DATA_PIN5": f"PIN{data_pins4567[1]}_bm",
-            "LCD_DATA_PIN6": f"PIN{data_pins4567[2]}_bm",
-            "LCD_DATA_PIN7": f"PIN{data_pins4567[3]}_bm",
-            "LCD_E_PORT": e_port,
-            "LCD_E_PIN": f"PIN{e_pin}_bm",
-            "LCD_RS_PORT": rs_port,
-            "LCD_RS_PIN": f"PIN{rs_pin}_bm",
-        }
-    )
+    dico = collections.OrderedDict()
     dico["LCDPREFIX"] = prefix
-
 
     filename = LIBDIR + "/lcd4." + ("h" if header else "c")
     with open(filename) as f:
@@ -46,6 +25,32 @@ def lcd4(
 
     content = "\n".join(filtered)
 
+    constants = collections.OrderedDict(
+        {
+            "LCD_DATA_PORT": data_port,
+            "LCD_DATA_PIN4": f"PIN{data_pins4567[0]}_bm",
+            "LCD_DATA_PIN5": f"PIN{data_pins4567[1]}_bm",
+            "LCD_DATA_PIN6": f"PIN{data_pins4567[2]}_bm",
+            "LCD_DATA_PIN7": f"PIN{data_pins4567[3]}_bm",
+            "LCD_E_PORT": e_port,
+            "LCD_E_PIN": f"PIN{e_pin}_bm",
+            "LCD_RS_PORT": rs_port,
+            "LCD_RS_PIN": f"PIN{rs_pin}_bm",
+            "LCD_CMD_LINEONE": "0x00",
+            "LCD_CMD_LINETWO": "0x40",
+            "LCD_CMD_CLEAR": "0b00000001",
+            "LCD_CMD_HOME": "0b00000010",
+            "LCD_CMD_ENTRYMODE": "0b00000110",
+            "LCD_CMD_DISPLAYOFF": "0b00001000",
+            "LCD_CMD_DISPLAYON": "0b00001100",
+            "LCD_CMD_FUNCTIONRESET": "0b00110000",
+            "LCD_CMD_FUNCTIONSET4BIT": "0b00101000",
+            "LCD_CMD_SETCURSOR": "0b10000000",
+        }
+    )
+
+    defined, content = generate_define("LCD", prefix, constants, content)
+
     if header:
-        return protect_header(f"LCD4_{prefix}", content)
+        return protect_header(f"LCD4_{prefix}", defined + "\n" + content)
     return content
